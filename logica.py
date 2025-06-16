72886ca366b3c6b802e5ff5e18348b598973cad3
Gustavo Developer <Kleversongustavo0@gmail.com>
2025-06-16 19:21:17 -0300

    Update logica.py

--- logica.py
+++ logica.py
@@ -1,8 +1,15 @@
-nota = float(input("Digite sua nota: "))
+from datetime import datetime
 
-if nota >= 7:
-    print("Aprovado!")
-elif nota >= 5:
-    print("Recuperação.")
+nome = input("Digite seu nome: ");
+print(f"olá {nome} bem vindo(a)"); 
+
+horas = datetime.now().strftime("%H:%M:%S")
+print(f"{nome} As horas são - {horas} ")
+
+idade = int(input(f"Quantos anos você tem {nome}: "));
+if idade >= 18:
+    print(f"{nome} Liberado");
+elif idade >= 17:
+    print(f"Acesso negado {nome}");
 else:
-    print("Reprovado.")
+    print(f"PROIBIDO A ENTRADA DE MENORES!")
